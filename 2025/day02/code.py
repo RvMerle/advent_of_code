8aef58df5d8b9c26299f6b01cf7cc3685dd280e6
Robin van Merle <8127492+RvMerle@users.noreply.github.com>
2025-12-02 19:27:42 +0100

    2025 day 2

---
+++ 2025/day02/code.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+
+def part1(line: str) -> int:
+    parts = line.split(",")
+    ids = []
+
+    for part in parts:
+        start, end = part.split("-")
+
+        for i in range(int(start), int(end) + 1):
+            s = str(i)
+            if len(s) % 2:
+                # ID needs to have even length to repeat itself
+                continue
+
+            # Check if first half equals second half
+            mid = len(s) // 2
+            if s[:mid] == s[mid:]:
+                ids.append(i)
+
+    return sum(ids)
+
+
+def part2(line: str) -> int:
+    parts = line.split(",")
+    ids = []
+
+    for part in parts:
+        start, end = part.split("-")
+
+        for i in range(int(start), int(end) + 1):
+            s = str(i)
+            for part_length in range(1, len(s) // 2 + 1):
+                # Try all lengths of sequences (1, 2, etc.)
+
+                if len(s) % part_length:
+                    # Original ID is not divisible in equal parts of length part_length
+                    continue
+
+                # Check if each part is equal
+                parts = [
+                    s[j * part_length : (j + 1) * part_length]
+                    for j in range(len(s) // part_length)
+                ]
+                if all(part == parts[0] for part in parts):
+                    ids.append(i)
+                    break
+
+    return sum(ids)
+
+
+if __name__ == "__main__":
+    with open(Path(__file__).parent / "input.txt") as f:
+        line = f.readline()
+    # line = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
+    print("Part 1:", part1(line))
+    print("Part 2:", part2(line))
